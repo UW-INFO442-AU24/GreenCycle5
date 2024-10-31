6f7f14eed98cf7adc98866b5407b389386ec065d
Paul Gialis <paulmg@uw.edu>
2024-10-31 12:55:07 -0700

    map framework

--- greencycle/src/App.js
+++ greencycle/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import NavBar from './components/NavBar/navbar.js'
-import Home from './components/Home/home.js'
-import About from './components/About/about.js'
-import Map from './components/Account/map.js'
-import Quiz from './components/Quiz/quiz.js'
-import Community from './components/Community/community.js'
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom"
+import NavBar from './components/NavBar/navbar.js';
+import Home from './components/Home/home.js';
+import About from './components/About/about.js';
+import Map from './components/Account/map.js';
+import Quiz from './components/Quiz/quiz.js';
+import Community from './components/Community/community.js';
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import 'leaflet/dist/leaflet.css';
 
 export default function App(props) {
   return (
