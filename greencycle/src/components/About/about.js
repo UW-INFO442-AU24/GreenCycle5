c2fdca924de119f79d1435aeeaa1f97899503f3c
carl-searle <carl1298@outlook.com>
2024-12-03 05:52:11 -0800

    added multimedia element

--- greencycle/src/components/About/about.js
+++ greencycle/src/components/About/about.js
@@ -8,8 +8,8 @@ export default function About(props) {
                 communities: Recycling. Recycling is essential to a sustainable future, but we have 
                 observed challenges to recycling that make it hard for people to participate.
                 Confusion on varying recycling rules as well as lack of knowledge on where to deposit
-                recyclable items can discourage people from contributing. That's why we have made King County
-                recycling information available in this singular platform.</p>
+                recyclable items can discourage people from contributing. That's why we've made King County
+                recycling information available on this singular platform.</p>
             <h1>Our Team:</h1>
             <p>Kevin Nguyen, Carl Searle, Kaylina Saetern, Paul Gialis, Jayden Set</p>
             <iframe id="recycling" width="600" height="415" src="https://www.youtube.com/embed/b7GMpjx2jDQ"></iframe>
